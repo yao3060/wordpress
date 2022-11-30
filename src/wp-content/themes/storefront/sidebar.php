318bdcdae643a0166322fd6468ef77a1353af4f5
YAO RMBP <yao3060@gmail.com>
2022-11-30 16:42:04 +0800

    add wpml for test

--- src/wp-content/themes/storefront/sidebar.php
+++ src/wp-content/themes/storefront/sidebar.php
@@ -1,15 +1,24 @@
 <?php
+
 /**
  * The sidebar containing the main widget area.
  *
  * @package storefront
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
+if (!is_active_sidebar('sidebar-1')) {
+    return;
 }
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+    <pre style="padding: 5px;">
+<?php
+echo ('get_user_locale:' . get_user_locale()) . PHP_EOL;
+echo ('get_locale:' . get_locale()) . PHP_EOL;
+echo ('WPML:' . apply_filters('wpml_current_language', NULL)) . PHP_EOL;
+?>
+</pre>
+
+    <?php dynamic_sidebar('sidebar-1'); ?>
 </div><!-- #secondary -->
